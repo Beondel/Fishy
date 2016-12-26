fdefbe833cc59527d5f0b71713851ea5858b9780
Beondel <bengmacmillan@gmail.com>
2016-12-26 16:06:47 -0500

    got rid of comments

--- src/Fish.java
+++ src/Fish.java
@@ -2,7 +2,4 @@
  * Created by benjaminmacmillan1 on 12/4/16.
  */
 public class Fish extends Ocean{
-    //simultaneous test
-
-    // You're pretty cute - test trying to change two classes at once
 }
