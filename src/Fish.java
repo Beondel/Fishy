73b905a65a2b81dac3c1f74ee8b32537d44fe0b9
siyukw <siyukw@gmail.com>
2017-01-01 14:52:26 -0800

    changed fish

--- src/Fish.java
+++ src/Fish.java
@@ -1,32 +1,39 @@
-import java.awt.*;
+import java.util.*;
 
-/**
- * Created by benjaminmacmillan1 on 12/4/16.
- */
 public class Fish {
     private int speed;
     private int size;
     private int position;
     private boolean alive;
+    private Random r;
 
     public Fish(int speed, int size, int position, boolean alive){
         this.speed = speed;
         this.size = size;
         this.position = position;
-        this.alive = alive;
+        this.alive = true;
     }
 
-    public boolean eat(){
-        return true;
+    public boolean getStatus() {
+        return alive;
     }
 
-    public int behavior(){
+    public int getSpeed() {
+        // speed is -50 to 50
+        int speed = r.nextInt(6) * 20 - 50;
         return speed;
 
     }
 
-    public int appearence(){
+    public int getSize() {
+        // size is 10 to 90
+        int size = r.nextInt(11) * 8 + 10;
         return size;
+    }
 
+    public int getPosition() {
+        // y is 10 to 720
+        int position = r.nextInt(72) * 10 + 10;
+        return position;
     }
 }
