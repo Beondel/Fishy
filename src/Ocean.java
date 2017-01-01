242bff4210fa0cead7c9fa4fc82b58bd3e706b04
siyukw <siyukw@gmail.com>
2017-01-01 15:21:38 -0800

    added fish on dp, in case crash

--- src/Ocean.java
+++ src/Ocean.java
@@ -12,14 +12,26 @@ public class Ocean {
 
         oceanBg(g, p);
 
-        Fish bob = new Fish(2, 20, 100, true);
+
+        Random r = new Random();
+        // speed is -50 to 50
+        int speed = r.nextInt(6) * 20 - 50;
+        // size is 10 to 90
+        int size = r.nextInt(11) * 8 + 10;
+        // y is 10 to 720
+        int position = r.nextInt(72) * 10 + 10;
+
+        Fish bob = new Fish(speed, size, position, true);
+
+        // draw the fish
+        drawFish(g, p, bob.getSpeed(), bob.getPosition(), bob.getSize(), bob.getStatus());
 
         Player person = new Player();
         //playerControl(person.getSize(), );
 
-        while(person.getStatus()){
+        //while(person.isAlive()){
 
-        }
+        //}
     }
 
     // background ocean, has nothing to do with player
@@ -42,6 +54,28 @@ public class Ocean {
         g.fillOval(x, y, size, size);
         g.setColor(Color.WHITE);
         g.fillOval(x + size / 5, y + size / 5, size / 4, size / 4);
+    }
+
+    //draw fish
+    public static void drawFish(Graphics g, DrawingPanel p, int speed, int y, int size, boolean alive){
+
+        int x = 0;
+        if(speed < 0){
+            x = 1200;
+        }
+        int oppX = 1200 - x;
+        while(alive && Math.abs(oppX - x) > Math.abs(speed)) {
+            x = x + speed;
+            g.setColor(Color.RED);
+            g.fillOval(x - size, y - size / 2, size * 2, size);
+            //g.fillRect(x, y - size / 4, size, size / 2);
+            //g.setColor(Color.BLACK);
+            //g.fillOval(x - size / 2, y, size / 8, size / 8);
+            p.sleep(500);
+            g.setColor(Color.BLUE);
+            g.fillOval(x - size, y - size / 2, size * 2, size);
+
+        }
     }
 
     //play control class
