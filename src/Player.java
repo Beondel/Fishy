1be0cb421d565ae0efabd404dcee3904c780c4f4
Beondel <bengmacmillan@gmail.com>
2017-01-01 15:36:52 -0800

    made player's alive and size fields public

--- src/Player.java
+++ src/Player.java
@@ -2,19 +2,11 @@
  * Created by benjaminmacmillan1 on 12/6/16.
  */
 public class Player {
-    private boolean isAlive;
-    private int size;
+    boolean isAlive;
+    int size;
 
     public Player(){
         this.isAlive = true;
         this.size = 50;
-    }
-
-    public boolean getStatus(){
-        return isAlive;
-    }
-
-    public int getSize(){
-        return size;
     }
 }
