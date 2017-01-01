824c35d05febc4a9e56b1229285ad87830f34aff
siyukw <siyukw@gmail.com>
2017-01-01 14:08:44 -0800

    added stuff in main

--- src/Player.java
+++ src/Player.java
@@ -3,12 +3,18 @@
  */
 public class Player {
     private boolean isAlive;
+    private int size;
 
     public Player(){
         this.isAlive = true;
+        this.size = 50;
     }
 
     public boolean getStatus(){
         return isAlive;
+    }
+
+    public int getSize(){
+        return size;
     }
 }
