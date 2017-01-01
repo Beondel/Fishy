6b1175ba8afef6c17002937675712daec1022bf5
Beondel <bengmacmillan@gmail.com>
2017-01-01 16:46:11 -0500

    skeleton of fish and player

--- src/Fish.java
+++ src/Fish.java
@@ -1,5 +1,29 @@
+import java.awt.*;
+
 /**
  * Created by benjaminmacmillan1 on 12/4/16.
  */
 public class Fish extends Ocean{
+    private int speed;
+    private int size;
+    private int position;
+    private boolean alive;
+
+    public Fish(int speed, int size, int position, boolean alive){
+        this.speed = speed;
+        this.size = size;
+        this.position = position;
+        this.alive = alive;
+    }
+
+    public boolean eat(){
+    }
+
+    public int behavior(){
+
+    }
+
+    public Graphics appearence(){
+
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -2,4 +2,9 @@
  * Created by benjaminmacmillan1 on 12/6/16.
  */
 public class Player extends Fish{
+    private boolean isAlive;
+
+    public Player(){
+        this.isAlive = true;
+    }
 }
